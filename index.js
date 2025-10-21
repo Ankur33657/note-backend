fb0e1c3a4bfacd648936c80f88b7a82f5b4a7f64
Ankur33657 <ankursingh943040@gmail.com>
2025-10-21 15:19:36 +0530

    feat:update cookies

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "https://note-frontend-37dy.onrender.com",
-      "https://note-frontend-k4wy.vercel.app",
-      "http://localhost:3000",
-    ],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
